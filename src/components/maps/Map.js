9caa7054ec808b8acfd010efd7bfa6e553f5b9da
Dalius Pamparas <dalius.pamparas@gmail.com>
2018-03-21 09:05:21 +0100

    fetch from api , first test

---
+++ src/components/maps/Map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Map = () => (
+    <div className='ui container'>
+        <h1>
+           Mapp
+           </h1>
+        MAP
+        <LoginForm/>
+   </div>
+   );
+   export default Map;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import{Link} from 'react-router-dom';
+import Map from '../maps/Map';
+import UserApiBild from '../api/UserApiBild'
 
 const HomePage = () => (
  <div>
      Här är home page<br/>
      <Link to="/Login">Login</Link>
-
+     <Map />
+     <UserApiBild/>
 </div>
 );
 
